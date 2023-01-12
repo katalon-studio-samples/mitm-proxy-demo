a1c931828d189be8a600f4894d50bc7a7ff1b919
Coty Rosenblath <coty@pobox.com>
2023-01-12 17:04:12 +0700

    Update New In-process Proxy Suite

--- Test Suites/New In-process Proxy Suite.ts
+++ Test Suites/New In-process Proxy Suite.ts
@@ -5,17 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
+   <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>c823f4c6-6f3f-4a81-b858-6cc062df4dc6</testSuiteGuid>
    <testCaseLink>
-      <guid>536f84b9-046b-4e6b-8b43-7818dcc2bf05</guid>
+      <guid>74c9c510-f447-460c-8098-96fba6383eab</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Proxy Test</testCaseId>
+      <testCaseId>Test Cases/Chromium Proxy Test</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
